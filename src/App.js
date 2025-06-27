2476d53ac211015b508d779d7a6984daa2826b9d
Kaviya <kaviyapothuavn@gmail.com>
2025-06-27 15:44:38 +0530

    Remove node_modules and add .gitignore

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to Solace 🌿</h1>
+      <p>Your safe space for healing and comfort.</p>
+    </div>
+  );
+}
+
+export default App;
